12d2ad3f338ffd80dbaa3d770bad62d2cceb2d0a
blasson42 <brycelasson@gmail.com>
2025-02-25 21:36:53 -0700

    Updated joinGame page to contain placeholder logic

--- src/home/joinGame.jsx
+++ src/home/joinGame.jsx
@@ -1,19 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useGame } from "../customContext/gameContext";
+import useGameLogic from "../customHooks/useGameLogic";
 
 export default function JoinGame() {
     const navigate = useNavigate();
+    const [joinCode, setJoinCode] = useState("");
+    const [error, setError] = useState(null);
+    const { joinGame, isProcessing } = useGameLogic();
+    const { user } = useGame();
+
+    const handleJoin = () => {
+        const codeString = joinCode.toString();
+
+        if (codeString.length !== 4) {
+            setError("Please enter a valid 4-digit code.");
+            return;
+        }
+        
+        setError(null);
+        
+        joinGame(joinCode, user);
+        
+        navigate("/home/waiting-room");
+    };
 
     return (
         <main>
             <section>
                 <p>Enter the code from your host to join their game.</p>
                 <div className="form-field">
-                    <input className="user-input" type="number" id="joinCodeField" name="joinCodeValue" placeholder="ie 5296" required />
+                    <input className="user-input" type="number" placeholder="ie 5296" required value={joinCode} onChange={(e) => setJoinCode(e.target.value)} />
                 </div>
                 <br />
-                <button className="submit-vote" onClick={() => { navigate('/home/waiting-room') }} style={{ width: 'auto' }}>Join Game</button>
+                <button className="submit-vote" onClick={handleJoin} style={{ width: 'auto' }} disabled={isProcessing}>Join Game</button>
             </section>
+            {error && <p className="error">{error}</p>}
         </main>
     )
 }
