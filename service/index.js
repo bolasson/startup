358b4fcd8de6dbd47be81b1449d98855ad5d35b8
blasson42 <brycelasson@gmail.com>
2025-03-13 14:49:43 -0600

    Added the public directory for the React build

--- service/index.js
+++ service/index.js
@@ -3,8 +3,14 @@ const app = express();
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
+app.use(express.static('public'));
+
 app.get('*', (_req, res) => {
   res.send({ msg: 'Backend service is running.' });
+});
+
+app.use((_req, res) => {
+    res.sendFile('index.html', { root: 'public' });
 });
 
 app.listen(port, () => {
