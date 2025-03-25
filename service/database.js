c27c8481e47b41ec3c188d349405f2304d331c4e
blasson42 <brycelasson@gmail.com>
2025-03-25 16:40:33 -0600

    Updated database modules

--- service/database.js
+++ service/database.js
@@ -5,8 +5,10 @@ const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostna
 
 const client = new MongoClient(url);
 const db = client.db('RankIt');
+const userCollection = db.collection('users');
+const gameCollection = db.collection('games');
 
-async function connectionTest() {
+(async function connectionTest() {
     try {
         await db.command({ ping: 1 });
         console.log(`Connected to the database`);
@@ -14,44 +16,68 @@ async function connectionTest() {
         console.error(e);
         process.exit(1);
     }
+})();
+
+async function createUser(user) {
+    await userCollection.insertOne(user);
 }
 
-async function addStaticUser() {
-    try {
-        const collection = db.collection('users');
-        const user = {
-            username: "test",
-            password: "toomanysecrets",
-            name: "Test",
-            stats: {
-                "Rounds Played": 0,
-                "Total Points Scored": 0,
-                "Date Joined": new Date().toLocaleDateString(),
-                "Last Played": new Date().toLocaleDateString(),
-            },
-        };
-        
-        await collection.insertOne(user);
-        const users = await collection.find({}).toArray();
-        users.forEach(user => console.log(user));
-
-        await closeConnection();
-    } catch (e) {
-        console.error(e);
-        process.exit(1);
-    }
+function getUser(username) {
+    return userCollection.findOne(
+        { username: username },
+        { projection: { password: 0 } }
+    );
 }
 
-async function closeConnection() {
-    try {
-        await client.close();
-        console.log('Connection closed');
-        process.exit(0);
-    } catch (e) {
-        console.error('Error closing the connection:', e);
-        process.exit(1);
-    }
+function getUserByToken(token) {
+    return userCollection.findOne(
+        { token: token },
+        { projection: { password: 0 } }
+    );
 }
 
-connectionTest();
-addStaticUser();
+async function updateUser(user) {
+    await userCollection.updateOne({ username: user.username }, { $set: user });
+}
+
+async function createGame(game) {
+    await gameCollection.insertOne(game);
+}
+
+function getGame(gameID) {
+    return gameCollection.findOne({ gameID: gameID });
+}
+
+async function updateGame(game) {
+    await gameCollection.updateOne({ gameID: game.gameID }, { $set: game });
+}
+
+async function deleteGame(gameID) {
+    await gameCollection.deleteOne({ gameID: gameID });
+}
+
+function getHighScores() {
+    const query = { "stats.Total Points Scored": { $gt: 0 } };
+    const options = {
+        sort: { "stats.Total Points Scored": -1 },
+        limit: 5,
+        projection: {
+            username: 1,
+            "stats.Total Points Scored": 1,
+        },
+    };
+    const cursor = userCollection.find(query, options);
+    return cursor.toArray();
+}
+
+module.exports = {
+    createUser,
+    getUser,
+    getUserByToken,
+    updateUser,
+    createGame,
+    getGame,
+    updateGame,
+    deleteGame,
+    getHighScores,
+};
