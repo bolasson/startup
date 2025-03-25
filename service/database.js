81db0c2bbbd73bd6a41b77918e0cec4191e36854
blasson42 <brycelasson@gmail.com>
2025-03-24 23:27:32 -0600

    Created addStaticUser to get comfortable with the database connections

--- service/database.js
+++ service/database.js
@@ -16,4 +16,42 @@ async function connectionTest() {
     }
 }
 
+async function addStaticUser() {
+    try {
+        const collection = db.collection('users');
+        const user = {
+            username: "test",
+            password: "toomanysecrets",
+            name: "Test",
+            stats: {
+                "Rounds Played": 0,
+                "Total Points Scored": 0,
+                "Date Joined": new Date().toLocaleDateString(),
+                "Last Played": new Date().toLocaleDateString(),
+            },
+        };
+        
+        await collection.insertOne(user);
+        const users = await collection.find({}).toArray();
+        users.forEach(user => console.log(user));
+
+        await closeConnection();
+    } catch (e) {
+        console.error(e);
+        process.exit(1);
+    }
+}
+
+async function closeConnection() {
+    try {
+        await client.close();
+        console.log('Connection closed');
+        process.exit(0);
+    } catch (e) {
+        console.error('Error closing the connection:', e);
+        process.exit(1);
+    }
+}
+
 connectionTest();
+addStaticUser();
