08790b218b15aa1fd759238f04ec18d3b1e17dfc
blasson42 <brycelasson@gmail.com>
2025-03-25 21:38:52 -0600

    Removed more old unused code

--- src/customContext/gameContext.jsx
+++ src/customContext/gameContext.jsx
@@ -40,33 +40,3 @@ export function GameProvider({ children }) {
 export function useGame() {
     return useContext(GameContext);
 }
-
-function validateCredentials(username, password, name) {
-
-    if (!username) {
-        return "A username is required.";
-    }
-
-    if (!password) {
-        return "A password is required.";
-    }
-
-    if (!name) {
-        return "Your name is required.";
-    }
-
-    const nameRegex = /^[a-zA-Z0-9._]+$/;
-    if (!nameRegex.test(name)) {
-        return "Display name may only contain numbers, letters, periods, and/or underscores.";
-    }
-
-    if (!nameRegex.test(username)) {
-        return "Username may only contain numbers, letters, periods, and/or underscores.";
-    }
-
-    if (password.length < 12) {
-        return "Password must be at least 12 characters long.";
-    }
-
-    return null;
-}
